1d379586e7b476b8f32425b419b8e8b299982055
ayushrockss <61458466+ayushrockss@users.noreply.github.com>
2024-03-15 20:01:02 +0530

    Add files via upload

---
+++ Day4/Static/src/com/java/ayush/staticdemo/empdriver.java
@@ -0,0 +1,13 @@
+package com.java.ayush.staticdemo;
+
+public class empdriver {
+
+	public static void main(String[] args) {
+		employee e1 = new employee(69, "Ayush");
+		System.out.println(e1);
+		
+		employee e2 = new employee(96, "Gauri");
+		System.out.println(e2);
+	}
+
+}
---
+++ Day4/Static/src/com/java/ayush/staticdemo/employee.java
@@ -0,0 +1,40 @@
+package com.java.ayush.staticdemo;
+
+public class employee {
+	private int empid;
+	private String name;
+	private static String companyName = "TNS";
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public static String getCompanyName() {
+		return companyName;
+	}
+	public static void setCompanyName(String companyName) {
+		employee.companyName = companyName;
+	}
+	public employee() {
+		super();
+	}
+	public employee(int empid, String name) {
+		super();
+		this.empid = empid;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "employee [empid=" + empid + ", name=" + name + ", Companyname=" + companyName + "]";
+	}
+			
+		
+
+}
